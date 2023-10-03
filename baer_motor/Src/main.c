ddc827919467c63b4da4e10bd8cffe1b6f3e6b75
hanliumaozhi <song.liu@ia.ac.cn>
2023-10-03 12:24:10 +0800

    commit

--- baer_motor/Src/main.c
+++ baer_motor/Src/main.c
@@ -157,12 +157,12 @@ void unpack_reply(FDCAN_RxHeaderTypeDef *pRxHeader, uint8_t *data)
 		}
 	}*/
 	
-	if (pRxHeader->DataLength == FDCAN_DLC_BYTES_7)
+	if (pRxHeader->DataLength == FDCAN_DLC_BYTES_8)
 	{
 		int id = pRxHeader->Identifier;
 		if (id > 0 && id < 7)
 		{
-			for (size_t i = 0; i < 7; i++)
+			for (size_t i = 0; i < 8; i++)
 			{
 				byte_8_reply.buffer[i] = data[i];
 			}
